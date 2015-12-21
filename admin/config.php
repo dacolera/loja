3e60218e9ac77ef27b9a631942fe30a5a1549fb5
dacolera <dacolera360@gmail.com>
2015-12-21 20:54:41 -0200

    commit inicial do projeto

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/loja/admin/');
+define('HTTP_CATALOG', 'http://localhost/loja/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/loja/admin/');
+define('HTTPS_CATALOG', 'http://localhost/loja/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/loja/admin/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/loja/system/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/loja/admin/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/loja/admin/view/template/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/loja/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/loja/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/loja/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/loja/system/storage/download/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/loja/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/loja/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/loja/system/storage/upload/');
+define('DIR_CATALOG', '/Applications/MAMP/htdocs/loja/catalog/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '2013');
+define('DB_DATABASE', 'loja');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/loja/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/loja/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/loja/catalog/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/loja/system/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/loja/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/loja/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/loja/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/loja/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/loja/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/loja/system/storage/download/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/loja/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/loja/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/loja/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '2013');
+define('DB_DATABASE', 'loja');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
